87776a904c7ca6c41ef89caa670e81d07167f529
ChedlyMar <57404850+ChedlyMar@users.noreply.github.com>
2021-06-30 08:43:01 +0100

    Update ToDoList.context.js

--- src/context/ToDoList.context.js
+++ src/context/ToDoList.context.js
@@ -3,7 +3,7 @@ import react, { createContext, useState } from "react";
 export const TodoListContext = createContext();
 
 export const TodoListProvider = (props) => {
-  const [toDoList, setTodoList] = useState(["chedly", "ala"]);
+  const [toDoList, setTodoList] = useState([]);
   return (
     <TodoListContext.Provider value={[toDoList, setTodoList]}>
       {props.children}
