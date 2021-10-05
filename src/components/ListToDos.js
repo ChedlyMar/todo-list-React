25c6312b67f40857ad805f98c39400b36c5c2e8f
ChedlyMar <57404850+ChedlyMar@users.noreply.github.com>
2021-10-05 08:00:53 +0100

    update list todo

--- src/components/ListToDos.js
+++ src/components/ListToDos.js
@@ -1,15 +1,36 @@
 import ToDo from "./ToDo";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const ListToDos = () => {
   const toDoList = useSelector((state) => state.tasks);
+  const [filtredList, setFiltredList] = useState(toDoList);
 
   return (
     <div>
+      <div>
+        <button className="" onClick={() => setFiltredList(toDoList)}>
+          All
+        </button>
+        <button
+          onClick={() =>
+            setFiltredList(toDoList.filter((todo) => todo.isDone === true))
+          }
+        >
+          Done
+        </button>
+        <button
+          onClick={() =>
+            setFiltredList(toDoList.filter((todo) => todo.isDone === false))
+          }
+        >
+          Not Done
+        </button>
+      </div>
       <h2 className="mt-6 mb-4 text-center text-3xl font-extrabold text-gray-900">
         To Do List
       </h2>
-      {toDoList.map((toDo) => {
+      {filtredList.map((toDo) => {
         return (
           <div key={toDo.id} className="">
             <div className="py-1">
